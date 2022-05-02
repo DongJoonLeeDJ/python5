04a4da1a00e4298953f5aed52f25e61b8f1d94f7
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-05-02 11:16:08 +0900

    220502
    220502

--- springboot/member/src/main/java/com/example/member/controller/MemberController.java
+++ springboot/member/src/main/java/com/example/member/controller/MemberController.java
@@ -4,10 +4,9 @@ import com.example.member.entity.Member;
 import com.example.member.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -18,25 +17,17 @@ public class MemberController {
     @Autowired
     MemberRepository memberRepository;
 
-    // insert
-    @GetMapping("add")
-    @CrossOrigin
-    public @ResponseBody String add(String email,String name,String password){
-        Member member = new Member();
-        member.setEmail(email);
-        member.setPassword(password);
-        member.setName(name);
+    @GetMapping("findall")
+    public String findall(Model model){
+        List<Member> list =  memberRepository.findAll();
 
-        memberRepository.save(member);
-        return "add";
+        model.addAttribute("list",list);
+        return "member/findall";
     }
 
-    // select
-    @GetMapping("all")
-    @CrossOrigin
-    public @ResponseBody List<Member> all(){
-        List<Member> list = memberRepository.findAll();
-        return list;
-    }
+    @GetMapping("insert")
+    public String insert(){
 
+        return "member/insert";
+    }
 }
