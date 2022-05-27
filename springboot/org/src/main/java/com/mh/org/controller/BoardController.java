96f10e7a2d093a6dc351bc36c2f48d95d5208e03
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-05-27 11:55:54 +0900

    220527
    220527

---
+++ springboot/org/src/main/java/com/mh/org/controller/BoardController.java
@@ -0,0 +1,15 @@
+package com.mh.org.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("freeboard")
+public class BoardController {
+
+    @GetMapping("select")
+    public String select(){
+        return "freeboard/select";
+    }
+}
